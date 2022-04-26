65a5357bf716f0132a077f114ff238ebc1b6ac42
Areslane <81231071+areslane67@users.noreply.github.com>
2022-04-26 14:34:46 +0100

    commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded");
+    let btn = document.querySelector(".burger");
+    let el = document.querySelector("header div");
+
+    btn.addEventListener("click", () => {
+        el.classList.toggle("open-menu");
+        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
+
+    });
+});
